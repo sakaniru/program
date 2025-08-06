a8ae3fbce5ed4582adfce719c20efaa416e06389
chang chiachi <a5698478@gmail.com>
2025-08-07 01:29:33 +0800

    日期裡面的更新

--- Z學習/日期.py
+++ Z學習/日期.py
@@ -1,7 +1,7 @@
 
 import datetime as dt
-#印出今天的日期
-# today=dt.date.today()
+# 印出今天的日期
+today=dt.date.today()
 # nextweek=  today + dt.timedelta(days=28)
 # print("今天的日期是:", today.strftime("%Y-%m-%d"))
 # print("今天是", today.year, "年", today.month, "月", today.day, "日")
@@ -9,6 +9,19 @@ import datetime as dt
 # print("四週後的日期是:", nextweek.strftime("%Y-%m-%d"))
 # print("四週後是", nextweek)
 # print("四週後是", nextweek.year, "年", nextweek.month, "月", nextweek.day, "日")
+#取得下個月第一天的日期
+next_month = today.replace(day=1) + dt.timedelta(days=32)
+next_month = next_month.replace(day=1)  # 確保是下個月的第一天
+
+#取得下個月最後一天的日期
+next_month_last_day = next_month + dt.timedelta(days=31)
+next_month_last_day = next_month_last_day.replace(day=1) - dt.timedelta(days=1)
+
+print("下個月第一天的日期是:", next_month.strftime("%Y-%m-%d"))
+print("下個月最後一天的日期是:", next_month_last_day.strftime("%Y-%m-%d"))
+print("今天的日期是:", today.strftime("%Y-%m-%d"))
+print("今天是", today.year, "年", today.month, "月", today.day, "日")
+# print("下個月第一天的日期是:", next_month.strftime("%Y-%m-%d"))
 weekday_name={
     0: "星期一",
     1: "星期二",
@@ -20,14 +33,14 @@ weekday_name={
 }
 
 
-today=dt.date.today()
-dtt=dt.date(today.year, today.month,1)
-while dtt.month == today.month:
-    weekday=dtt.weekday()
-    print(dtt, weekday_name[weekday])
-    if (dtt.weekday(), weekday_name[weekday]) == (6, "星期日"):
-        print("-"*30)
-    dtt += dt.timedelta(days=1)
+# today=dt.date.today()
+# dtt=dt.date(today.year, today.month,1)
+# while dtt.month == today.month:
+#     weekday=dtt.weekday()
+#     print(dtt, weekday_name[weekday])
+#     if (dtt.weekday(), weekday_name[weekday]) == (6, "星期日"):
+#         print("-"*30)
+#     dtt += dt.timedelta(days=1)
 
 
         
