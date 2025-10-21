464970aaaefc676bdfade2501747ac0db645ecf0
chang chiachi <a5698478@gmail.com>
2025-10-22 05:35:54 +0800

    更新資料、命名

--- 另外開發/API2.py
+++ 另外開發/API2.py
@@ -3,7 +3,7 @@
 # if __name__ == "__main__":
 #     uvicorn.run("APITEST:app", host="127.0.0.1", port=8000, reload=True)
 
-from APITEST import app
+from API1 import app
 import uvicorn
 
 if __name__ == "__main__":
